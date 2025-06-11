9a5dcbde6dcfa02839dc7db85cc5b0a15d98baa2
Yolanda Flores <121079000+yolandafloresc14@users.noreply.github.com>
2025-06-11 10:24:06 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Plan de Perú para reducir el parque de las Líneas de Nazca",
+"autor" => "Associated Press",
+"resumen" => "La decisión de Perú de reducir su parque arqueológico, hogar de las famosas Líneas de Nazca, en aproximadamente un 42 % (un área equivalente a aproximadamente 1.400 campos de fútbol).",
 ];
 ?>
