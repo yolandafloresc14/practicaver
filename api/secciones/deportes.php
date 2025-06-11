01a3895254e5d67ce7f8e1fe2a42fa526f13574c
Yolanda Flores <121079000+yolandafloresc14@users.noreply.github.com>
2025-06-11 10:22:48 -0500

    Update deportes.php

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $deportes = [
 "titulo" => "Mi ciclo en la selección peruana terminó",
 "autor" => "Gabriel Casimiro",
-"resumen" => "El pasado 29 de mayo, Miguel Trauco fue desconvocado de la Selección Peruana por una lesión que se produjo en el partido de Alianza Lima ante Libertad en Paraguay, que determinó la clasificación de los íntimos a la Copa Sudamericana.",
+"resumen" => "El pasado 29 de mayo, Miguel Trauco fue desconvocado de la Selección Peruana por una lesión que se produjo en el partido de Alianza Lima ante Libertad en Paraguay.",
 ];
 ?>
