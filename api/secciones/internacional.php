f058b6446b491fe6f63f63f6c6e644f25ac86829
Yolanda Flores <121079000+yolandafloresc14@users.noreply.github.com>
2025-06-11 10:16:56 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La pesadilla de Okaal para conseguir ayuda en Gaza",
+"autor" => "Lara Villalón Isabel González",
+"resumen" => "Convencido por su vecino, Ihab Okaal partió la noche del domingo en una ruta a pie hacia Tal al Sultan, en el sur de Gaza, en búsqueda de una de las cajas de alimentos que reparte la Fundación Humanitaria de Gaza (FHG), un mecanismo impulsado por Estados Unidos e Israel que reemplaza a la ONU en la entrega de ayuda. Desde la ciudad de Jan Yunis, donde Okaal vive en una tienda de campaña con su familia, son unas dos horas a pie hasta este campo de refugiados en Rafah, aunque el camino se hizo más largo ante la falta de electricidad, que impide ver unas calles desfiguradas por los bombardeos israelíes y repletas de escombros.",
 ];
 ?>
