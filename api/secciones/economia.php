d6446ffc7f54a26cdff1a608173afc9ae5082eb4
Yolanda Flores <121079000+yolandafloresc14@users.noreply.github.com>
2025-06-11 10:19:42 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Camposol, Beta, Danper y más grandes agroexportadoras se perfilan como las más beneficiadas",
+"autor" => "El Comercio",
+"resumen" => "El Pleno del Congreso aprobó en primera votación el dictamen de la nueva ley agraria (Ley Chlimper 2.0) pese a las consecutivas alertas del Consejo Fiscal.",
 ];
 ?>
