042e803b3c916de864925cbd1378341f28fc1f2e
Yolanda Flores <121079000+yolandafloresc14@users.noreply.github.com>
2025-06-11 10:12:53 -0500

    Add files via upload

---
+++ api/secciones/portada.php
@@ -0,0 +1,11 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$portada = [
+"titulo" => "Portada",
+"autor" => "jperez",
+"resumen" => "El resumen de resumenes",
+];
+?>
